cacee1bec56d55a110ec43d68cc904062989f1e2
TheWorstOne <mangelladen@gmail.com>
2019-05-31 17:18:05 -0500

    Add new awesome Electron-dev feature :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Home from './components/home/Home'
+import Home from './components/home/home'
 
 class App extends Component {
   render() {
